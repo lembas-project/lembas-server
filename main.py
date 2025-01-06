b1052e1e6d3ff6e2bab365c5a6c66b17f66a79b1
mattkram <mkramer@anaconda.com>
2025-01-06 14:46:36 -0600

    feat: Add default values for non-secret environment variables

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoin
 
 load_dotenv()
 
+# TODO: Implement proper settings class
 CLIENT_ID = os.environ["CLIENT_ID"]
 CLIENT_SECRET = os.environ["CLIENT_SECRET"]
-REDIRECT_URL = os.environ["REDIRECT_URL"]
-TOKEN_URL = os.environ["TOKEN_URL"]
-LOGIN_URL_BASE = os.environ["LOGIN_URL_BASE"]
+REDIRECT_URL = os.getenv("REDIRECT_URL", "http://lembas.localhost")
+TOKEN_URL = os.getenv("TOKEN_URL", "https://github.com/login/oauth/access_token")
+LOGIN_URL_BASE = os.getenv("LOGIN_URL_BASE", "https://github.com/login/oauth/authorize")
 LOGIN_URL = LOGIN_URL_BASE + "?" + urlencode(dict(client_id=CLIENT_ID, redirect_url=REDIRECT_URL))
 
 log = logging.getLogger(__name__)
