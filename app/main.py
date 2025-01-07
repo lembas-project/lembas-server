31f2daceeb46b1f6508c4506771322a5268b852a
mattkram <mkramer@anaconda.com>
2025-01-07 16:34:18 -0600

    refac: Use application factory pattern to make things easier to test

--- app/main.py
+++ app/main.py
@@ -5,13 +5,20 @@ from app import templates
 from app.routes import router
 from app.settings import Settings
 
-config = Settings()  # type: ignore[call-arg]
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory=config.static_dir), name="static")
+def create_app(config: Settings | None = None) -> FastAPI:
+    config = config or Settings()  # type: ignore[call-arg]
 
-templates.init_app(app, template_dir=config.template_dir)
-app.include_router(router)
+    app = FastAPI()
+    app.mount("/static", StaticFiles(directory=config.static_dir), name="static")
 
-# Mount the config to the app so we can inject it into requests
-app.extra["config"] = config
+    templates.init_app(app, template_dir=config.template_dir)
+    app.include_router(router)
+
+    # Mount the config to the app so we can inject it into requests
+    app.extra["config"] = config
+
+    return app
+
+
+app = create_app()
