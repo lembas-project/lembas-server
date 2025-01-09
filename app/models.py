6da845b513fe6cac2bdf3ffe9581165affab804d
mattkram <mkramer@anaconda.com>
2025-01-08 23:36:29 -0600

    refac: Rename Button -> LinkButton

--- app/models.py
+++ app/models.py
@@ -12,26 +12,18 @@ class Component(BaseModel):
         # We do a shallow dump to prevent serializing every nested thing into a dictionary
         shallow_dump = {name: getattr(self, name) for name in self.model_fields.keys()}
         # TODO: Passing in Button should be done via some type of registration process instead
-        return render_template(self.__template_path__, **shallow_dump, Button=Button)
+        return render_template(self.__template_path__, **shallow_dump, LinkButton=LinkButton)
 
 
-class Button(Component):
-    __template__ = """
-      <button
-        class="button-link px-6 py-3 bg-green-500 hover:bg-green-600 text-white
-               font-semibold rounded-lg transition duration-300 ease-in-out
-               transform hover:scale-105"
-        data-href="{{ url }}"
-      >
-        <a href="{{ url }}">{{ text }}</a>
-      </button>
-      """
+class LinkButton(Component):
+    __template_path__ = "partials/link_button.html"
+
     url: str = ""
     text: str = ""
 
 
 class User(Component):
-    __template_path__ = "partials/user.html"
+    __template_path__ = "partials/user_card.html"
 
     username: str = Field(validation_alias="login")
     name: str | None = None
