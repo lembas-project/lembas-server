987b99629ded1b4b633fc943a717b6abd09837be
mattkram <mkramer@anaconda.com>
2025-01-10 14:40:53 -0600

    tidy: Lump everything into single registration function

--- app/jinja_partials.py
+++ app/jinja_partials.py
@@ -9,28 +9,22 @@ if TYPE_CHECKING:
 
 __all__ = [
     "register_starlette_extensions",
-    "render_partial",
 ]
 
 
-def render_partial(
-    template_name: str,
-    renderer: Callable[..., Any],
-    markup: bool = True,
-    **data: Any,
-) -> Markup | str:
-    if markup:
-        return Markup(renderer(template_name, **data))
-
-    return renderer(template_name, **data)
-
-
-def generate_render_partial(renderer: Callable[..., Any]) -> Callable[..., Markup | str]:
-    return partial(render_partial, renderer=renderer)
-
-
 def register_starlette_extensions(templates: "Jinja2Templates") -> None:
+    def render_partial(
+        template_name: str,
+        renderer: Callable[..., Any],
+        markup: bool = True,
+        **data: Any,
+    ) -> Markup | str:
+        if markup:
+            return Markup(renderer(template_name, **data))
+
+        return renderer(template_name, **data)
+
     def renderer(template_name: str, **data: Any) -> str:
         return templates.get_template(template_name).render(**data)
 
-    templates.env.globals.update(render_partial=generate_render_partial(renderer))
+    templates.env.globals.update(render_partial=partial(render_partial, renderer=renderer))
