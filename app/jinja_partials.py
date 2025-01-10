275aafa10bc0f27f7cf4f43026f0b8bc10977149
mattkram <mkramer@anaconda.com>
2025-01-10 14:00:44 -0600

    tidy: Remove unused code

--- app/jinja_partials.py
+++ app/jinja_partials.py
@@ -2,30 +2,14 @@ from collections.abc import Callable
 from functools import partial
 from typing import TYPE_CHECKING, Any
 
-from jinja2 import Environment
 from markupsafe import Markup as Markup
 
-try:
-    import flask
-except ImportError:
-    flask = None
-
-try:
-    import starlette
-except ImportError:
-    starlette = None
-
-if TYPE_CHECKING and flask and starlette:
-    from flask import Flask
-    from starlette.templating import Jinja2Templates
+if TYPE_CHECKING:
+    from fastapi.templating import Jinja2Templates
 
 __all__ = [
-    "register_extensions",
     "register_starlette_extensions",
-    "register_environment",
     "render_partial",
-    "generate_render_partial",
-    "PartialsException",
 ]
 
 
@@ -35,16 +19,10 @@ class PartialsException(Exception):
 
 def render_partial(
     template_name: str,
-    renderer: Callable[..., Any] | None = None,
+    renderer: Callable[..., Any],
     markup: bool = True,
     **data: Any,
 ) -> Markup | str:
-    if renderer is None:
-        if flask is None:
-            raise PartialsException("No renderer specified")
-        else:
-            renderer = flask.render_template
-
     if markup:
         return Markup(renderer(template_name, **data))
 
@@ -57,25 +35,8 @@ def generate_render_partial(
     return partial(render_partial, renderer=renderer, markup=markup)
 
 
-def register_extensions(app: "Flask"):
-    if flask is None:
-        raise PartialsException("Install Flask to use `register_extensions`")
-
-    app.jinja_env.globals.update(render_partial=generate_render_partial(flask.render_template))
-
-
-def register_starlette_extensions(templates: "Jinja2Templates"):
-    if starlette is None:
-        raise PartialsException("Install Starlette to use `register_starlette_extensions`")
-
+def register_starlette_extensions(templates: "Jinja2Templates") -> None:
     def renderer(template_name: str, **data: Any) -> str:
         return templates.get_template(template_name).render(**data)
 
     templates.env.globals.update(render_partial=generate_render_partial(renderer))
-
-
-def register_environment(env: Environment, markup: bool = False):
-    def renderer(template_name: str, **data: Any) -> str:
-        return env.get_template(template_name).render(**data)
-
-    env.globals.update(render_partial=generate_render_partial(renderer, markup=markup))
