f862669971e4adf5052c3ddf00008339e7e720af
mattkram <mkramer@anaconda.com>
2025-01-24 23:11:59 -0600

    fix: Follow redirect

--- tests/test_views.py
+++ tests/test_views.py
@@ -2,6 +2,6 @@ from httpx import AsyncClient
 
 
 async def test_get_home(client: AsyncClient) -> None:
-    response = await client.get("/")
+    response = await client.get("/", follow_redirects=True)
     assert response.status_code == 200
     assert "Lembas" in response.text
